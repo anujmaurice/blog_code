2fbb2cd16d39765e859e27812ac1a00dc4f77f52
anujmaurice <anuj.maurice@gmail.com>
2010-12-04 03:46:39 +0530

    first commit of src for blog

---
+++ example.archetype/example/archetype/browser/activityreport.py
@@ -0,0 +1,95 @@
+"""A report of recently modified cinemas and films
+"""
+
+from DateTime import DateTime
+
+from Acquisition import aq_inner
+
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from Products.CMFCore.utils import getToolByName
+
+from example.archetype.interfaces import IInstantMessage
+
+from plone.memoize.instance import memoize
+
+import pdb
+
+class ActivityReportView(BrowserView):
+    """View for showing recent cinema and film modifications
+    """
+        
+    template = ViewPageTemplateFile('activityreport.pt')
+        
+    def __call__(self):
+        # Hide the editable-object border
+        self.request.set('disable_border', True)
+
+        return self.template()
+        
+    def recently_modified_blogs(self):
+        context = aq_inner(self.context)
+        catalog = getToolByName(context, 'portal_catalog')
+        results = []
+	#pdb.set_trace()
+        for r in catalog(object_provides=IInstantMessage.__identifier__,
+                         modified=dict(query=self.modified_after(), range='min'),
+                         sort_on='modified',
+                         sort_order='reverse',):
+	    #pdb.set_trace()
+            results.append(dict(url=r.getURL(),
+                                title=r.Title,
+                                description=r.Description,
+				creator =r.Creator,
+				date = r.CreationDate,
+                                subject  = r.Subject))
+	print self.split_ofthree(results)
+        return self.split_ofthree(results)
+
+    def latest(self):
+        context = aq_inner(self.context)
+        catalog = getToolByName(context, 'portal_catalog')
+        results = []
+	#pdb.set_trace()
+        for r in catalog(object_provides=IInstantMessage.__identifier__,
+                         modified=dict(query=self.modified_after(), range='min'),
+                         sort_on='modified',
+                         sort_order='reverse',):
+	    #pdb.set_trace()
+            results.append(dict(url=r.getURL(),
+                                title=r.Title,
+                                description=r.Description,
+				creator =r.Creator,
+				date = r.CreationDate,
+                                subject  = r.Subject))
+        return results[0]
+        
+    def modified_after(self):
+	self.days = 70
+        return DateTime() - self.days
+
+    def split_ofthree(self,a):
+	i = 0
+	l = len(a)
+	aa=[]
+	while i < l:
+		aa.append(self.dict_assign(a.__getslice__(i,i+3)))
+		i = i+3
+	return aa
+
+    def dict_assign(self,ww):
+	dict = {}
+	try:
+		dict["1"]= ww[0]
+		dict["2"] = ww[1]
+		dict["3"] =ww[2]
+	except:
+            if len(dict.keys())==2:
+                dict["3"]=""
+            else:
+                dict["2"]=""
+                dict["3"]=""
+	return dict
+        
+ 
---
+++ example.archetype/example/archetype/interfaces.py
@@ -0,0 +1,21 @@
+from zope.interface import Interface
+
+from plone.theme.interfaces import IDefaultPloneLayer
+from zope import schema
+
+class IInstantMessage(Interface):
+    """Marker interface
+    """
+
+class IBiofooter(Interface):
+    """Marker interface
+    """
+    username = schema.ASCIILine(title=u"User name",description=u"The database user name",required=True)
+    password = schema.Password(title=u"Password",description=u"The database password",required=False)
+    database = schema.ASCIILine(title=u"Database name",description=u"The name of the database on this server", required=True)
+
+
+class IInstantMessageSpecific(IDefaultPloneLayer):
+    """Marker interface that defines a Zope 3 skin layer 
+       for this product.
+    """
